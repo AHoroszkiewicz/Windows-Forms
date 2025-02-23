15b06ddddcc84945c207296918f77822cfd34a5e
Adam Horo <loginadas@gmail.com>
2025-02-23 12:38:58 +0100

    temat 2

---
+++ temat 2/Kolory/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kolory
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1_ValueChanged(this, null);
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            panel1.BackColor = Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Escape)
+            {
+                Close();
+            }
+        }
+    }
+}
