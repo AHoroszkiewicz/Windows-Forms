114696427616842326d4e93a77a73f523533d364
Adam Horo <loginadas@gmail.com>
2025-04-13 13:04:22 +0200

    temat 6 part 1

---
+++ temat 6/firma/FakturaSprzedazy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq.Mapping;
+
+namespace firma
+{
+    [Table(Name = "FakturySprzedazy")]
+    internal class FakturaSprzedazy
+    {
+        [Column(Name = "Id", IsPrimaryKey = true, CanBeNull = false)] public int Id;
+        [Column(CanBeNull = false)] public string Numer;
+        [Column(CanBeNull = false)] public double Netto;
+        [Column(CanBeNull = true)] public double? Vat;
+        [Column(CanBeNull = false)] public DateTime Data;
+        [Column(CanBeNull = true)] public double? Zaplacono;
+        [Column(CanBeNull = false)] public int KontrahentId;
+        [Column(CanBeNull = false)] public int PracownikId;
+    }
+}
---
+++ temat 6/firma/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Linq;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace firma
+{
+    public partial class Form1 : Form
+    {
+        const string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\Studia\Windows Forms\temat 6\firma\firma.mdf;Integrated Security=True;Connect Timeout=30";
+        static DataContext bazaDanychFirma = new DataContext(connectionString);
+        static Table<Pracownik> listaPracownikow = bazaDanychFirma.GetTable<Pracownik>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string s = "Lista Pracowników: " + "\n";
+            var lp = from Pracownik in listaPracownikow select Pracownik;
+            foreach (var p in lp)
+            {
+                s += p.Imię + " "+ p.Nazwisko + " " + p.Email + " " + p.Telefon + "\n";
+            }
+            MessageBox.Show(s);
+        }
+    }
+}
